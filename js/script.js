822fdb84a8b73a053be5b37616c1d82a6661f9a1
Kiprotich Dominic Korir <kiprotichdominic102@gmail.com>
2019-10-25 15:02:35 +0300

    declare javascript arrays

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+//declare an array for my names
+var femaleAkan = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var maleAkan = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
+// a function that is called in html
+function printName() {
+	var year = document.getElementById("year").value;
+	var month = document.getElementById("month").value;
+	var day = document.getElementById("day").value;
+}
