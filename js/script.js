dbedb957eea9513d3ac2b9dab772d9846bcf6f32
Kiprotich Dominic Korir <kiprotichdominic102@gmail.com>
2019-10-26 10:59:38 +0300

    modify javascript functions

--- js/script.js
+++ js/script.js
@@ -27,14 +27,26 @@ var daysWeek = [
 	"Friday",
 	"Saturday"
 ];
-function getMyAkanName() {
+// declare a function
+function getYourkanName() {
 	var year = parseInt(document.getElementById("year").value);
 	var month = parseInt(document.getElementById("month").value);
 	var day = parseInt(document.getElementById("day").value);
 	var gender = getGender();
 	var date = new date(year + "/" + month + "/" + day);
-	var dateOfBirth = date.getDay();
-	var akanName;
+	var dateOfBirth = date.getDaysWeek();
+	var getYourkanName;
+
+	if (gender === "male") {
+		getYourkanName = maleAkanName[dateOfBirth];
+	} else {
+		getYourkanName = femaleAkanName[dateOfBirth];
+	}
+	alert(
+		"Awesome! You were born on " +
+			daysWeek[dateOfBirth] + 
+			" and your Akan name is " +
+			getYourkanName
+	);
 }
-alert("You were born on" + daysWeek[dateOfBirth] + "and");
 
