ab9e61041586194402ebbde09f4eea375b918841
Kiprotich Dominic Korir <kiprotichdominic102@gmail.com>
2019-10-26 12:18:20 +0300

    add a success button, and javscript to hide and display it

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//hide the alert for name
+document.getElementById("alert1").style.display = "none";
 //declare an array for my names
 var femaleAkanName = [
 	"Akosua",
@@ -28,25 +30,34 @@ var daysWeek = [
 	"Saturday"
 ];
 // declare a function
-function getYourkanName() {
+function getYourAkanName() {
 	var year = parseInt(document.getElementById("year").value);
 	var month = parseInt(document.getElementById("month").value);
 	var day = parseInt(document.getElementById("day").value);
 	var gender = getGender();
 	var date = new date(year + "/" + month + "/" + day);
 	var dateOfBirth = date.getDaysWeek();
-	var getYourkanName;
-
+	var getYourAkanName;
+	// conditions usimng if else statements
 	if (gender === "male") {
-		getYourkanName = maleAkanName[dateOfBirth];
+		getYourAkanName = maleAkanName[dateOfBirth];
 	} else {
-		getYourkanName = femaleAkanName[dateOfBirth];
+		getYourAkanName = femaleAkanName[dateOfBirth];
 	}
-	alert(
-		"Awesome! You were born on " +
-			daysWeek[dateOfBirth] + 
-			" and your Akan name is " +
-			getYourkanName
-	);
 }
 
+function getYourAkanName() {
+	document.getElementById("alert1").style.display = "block";
+	document.getElementById("span1").innerHTML = ""+daysWeek[dateOfBirth]+ "";
+	document.getElementById("span2").innerHTML = "dsksks";
+}
+
+function getGender() {
+	var yourGender = document.getElementsById("gender");
+
+	for (i = 0; i < yourGender.length; i++) {
+		if (yourGender[i].checked) {
+			return yourGender[i].value;
+		}
+	}
+}
