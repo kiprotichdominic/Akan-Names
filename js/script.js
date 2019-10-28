7dc43528d1768ada1c0b65566fbc6152286f17eb
Kiprotich Dominic Korir <kiprotichdominic102@gmail.com>
2019-10-28 11:52:26 +0300

    change link to images, change my js format

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
-//hide the alert for name
+//hide the alert button
 document.getElementById("alert1").style.display = "none";
+document.getElementById("alert2").style.display = "none";
 //declare an array for my names
 var femaleAkanNames = [
-	"Akosua, <img src='/images/akosua.jpg' height='200px'>",
-	"Adwoa, <img src='/images/adwoa.jpg' height='200px'>",
-	"Abenaa, <img src='/images/abena.jpeg' height='200px'>",
-	"Akua, <img src='/images/akua.jpeg' height='200px'>",
-	"Yaa, <img src='/images/yaa.jpeg' height='200px'>",
-	"Afua, <img src='/images/afua.jpg' height='200px'>",
-	"Ama, <img src='/images/ama.jpeg' height='200px'>"
+	"Akosua, <img src='https://bit.ly/2JsWQ5t' height='200px'>",
+	"Adwoa, <img src='https://bit.ly/369OsS7' height='200px'>",
+	"Abenaa, <img src='https://bit.ly/2MRihiN' height='200px'>",
+	"Akua, <img src='https://bit.ly/2Poskxu' height='200px'>",
+	"Yaa, <img src='https://bit.ly/2PoBN7T' height='200px'>",
+	"Afua, <img src='https://bit.ly/2pj47xU' height='200px'>",
+	"Ama, <img src='https://bit.ly/348GTcI' height='200px'>"
 ];
 var maleAkanNames = [
-	"Kwasi, <img src='/images/kwasi.jpg' height='200px'>",
-	"Kwadwo, <img src='/images/kwadwo.jpg' height='200px'>",
-	"Kwabena, <img src='/images/kwabena.jpeg' height='200px'>",
-	"Kwaku, <img src='/images/kwaku.jpg' height='200px'>",
-	"Yaw, <img src='/images/yaw.jpg' height='200px'>",
-	"Kofi, <img src='/images/kofi.jpeg' height='200px'>",
-	"Kwame, <img src='/images/kwame.jpeg' height='200px'>"
+	"Kwasi, <img src='https://bit.ly/2omOfdc' height='200px'>",
+	"Kwadwo, <img src='https://bit.ly/31SWwmV' height='200px'>",
+	"Kwabena, <img src='https://bit.ly/2PphxDi' height='200px'>",
+	"Kwaku, <img src='https://bit.ly/3683NCG' height='200px'>",
+	"Yaw, <img src='https://bit.ly/36eVwx0' height='200px'>",
+	"Kofi, <img src='https://bit.ly/2JsXMH1' height='200px'>",
+	"Kwame, <img src='https://bit.ly/2BRxDNG' height='200px'>"
 ];
 
 var daysWeek = [
@@ -29,37 +30,37 @@ var daysWeek = [
 	"Friday",
 	"Saturday"
 ];
-
-// declare a function
-function getYourAkanName() {
+//calling functions
+function getAkanName() {
 	var year = parseInt(document.getElementById("year").value);
 	var month = parseInt(document.getElementById("month").value);
 	var day = parseInt(document.getElementById("day").value);
-	var gender = getGender();
+	var male = document.getElementById("male");
+	var female = document.getElementById("female");
 
-	var day = new Date(year + "-" + month + "-" + day);
-
+	//validate date
+	if (day <= 0 || day >= 32) {
+		alert(
+			"Be serious, that can't be correct. Console your guardian or parent for correct !!!DATE!!!"
+		);
+	}
+	var day = new Date(year + "/" + month + "/" + day);
+	//storing date in variable date of birth
 	var dateOfBirth = day.getDay();
-
-	var akanName;
-
-	// conditions using if else statements when radio button is checked
-	if (gender === "male") {
-		akanName = maleAkanNames[dateOfBirth];
-	} else {
-		akanName = femaleAkanNames[dateOfBirth];
+	if (male.checked == true) {
+		document.getElementById("alert1").style.display = "block";
+		document.getElementById("span1").innerHTML =
+			"Awesome! You were born on, " + daysWeek[dateOfBirth];
+		document.getElementById("span2").innerHTML =
+			"your Akan Name is " + maleAkanNames[dateOfBirth];
+	} else if (female.checked == true) {
+		document.getElementById("alert1").style.display = "block";
+		document.getElementById("span1").innerHTML = daysWeek[dateOfBirth];
+		document.getElementById("span2").innerHTML = femaleAkanNames[dateOfBirth];
 	}
-	document.getElementById("alert1").style.display = "block";
-	document.getElementById("span1").innerHTML = daysWeek[dateOfBirth];
-	document.getElementById("span2").innerHTML = akanName;
 }
+//refresh button
 
-function getGender() {
-	var gender = document.getElementsByName("gender");
-
-	for (i = 0; i < gender.length; i++) {
-		if (gender[i].checked) {
-			return gender[i].value;
-		}
-	}
+function refreshPage() {
+	window.location.reload();
 }
